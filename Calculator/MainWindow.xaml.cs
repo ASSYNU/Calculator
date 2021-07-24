32f5ac0df7021ccdee7a38408016d12ecfe104d1
Assynu <dawid.bartczak05@gmail.com>
2021-07-24 21:30:51 +0200

    Added Negative Numbers

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -10,12 +10,9 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow
     {
-        private string[] input =
-        {
-            null,
-            null,
-            null,
-        };
+        private int[] input = new int[2];
+
+        private string _symbol;
         
         private int i = 0;
         private bool resultshow = false;
@@ -30,27 +27,26 @@ namespace Calculator
             Environment.Exit(0);
         }
 
-        private void UpdateMainText(string newData)
+        private void UpdateMainText(int newData)
         {
-            string content = "";
+            int content;
             if (!resultshow)
             {
-                input[i] += newData;
+                input[i] = input[i]*10+newData;
             }
             else
             {
                 input[i] = newData;
                 resultshow = false;
             }
-            if (input[i].Length <= 26)
+            if (input[i].ToString().Length <= 26)
             {
-                content = input[i];
+                Output.Text = input[i].ToString();
             }
             else
             {
-                content = "Too Long!";
+                Output.Text = "Too Long!";
             }
-            Output.Text = content;
         }
         
         // Top bar
@@ -67,7 +63,7 @@ namespace Calculator
         
         private void calculator_input(object sender, RoutedEventArgs e)
         {
-            string content = (sender as Button).Content.ToString();
+            int content = Int32.Parse((sender as Button).Content.ToString());
             UpdateMainText(content);
         }
 
@@ -75,15 +71,15 @@ namespace Calculator
         {
             if (i == 0)
             {
-                input[1] = (sender as Button).Content.ToString();
+               _symbol = (sender as Button).Content.ToString();
             }
             else
             {
-                input[0] = result().ToString();
-                input[1] = (sender as Button).Content.ToString();
+                input[0] = result();
+                _symbol = (sender as Button).Content.ToString();
             }
-            i = 2;
-            Output.Text = input[1];
+            i = 1;
+            Output.Text = _symbol;
             resultshow = false;
         }
 
@@ -92,56 +88,54 @@ namespace Calculator
             int result = this.result();
             Output.Text = result.ToString();
         }
+        
+        private void calculator_negative(object sender, RoutedEventArgs e)
+        {
+            input[i] = input[i] * (-1);
+            Output.Text = input[i].ToString();
+        }
 
         private int result()
         {                
             int result = 0;
             
-            if (input[0] != null && input[1] != null && input[2] != null)
+            if (input[0] != 0 && input[1] != 0)
             {
-                if (input[0] != "0" && input[2] != "0")
+                int input_1 = input[0];
+                int input_2 = input[1];
+                if (_symbol == "+")
                 {
-                    int input_1 = Int32.Parse(input[0]);
-                    int input_2 = Int32.Parse(input[2]);
-                    string symbol = input[1];
-                    if (symbol == "+")
-                    {
-                        result = (input_1 + input_2);
-                    }
-                    else if (symbol == "-")
-                    {
-                        result = (input_1 - input_2);
-                    }
-                    else if (symbol == "x")
-                    {
-                        result = (input_1 * input_2);
-                    }
-                    else if (symbol == "/")
-                    {
-                        result = (input_1 / input_2);
-                    }
-                    else
-                    {
-                        result = input_1;
-                    }
-
-                    input[1] = null;
-                
-                    for (int i = 0; i < input.Length; i++)
-                    {
-                        input[i] = null;
-                    }
-
-                    input[0] = result.ToString();
-
-                    i = 0;
-                
-                    resultshow = true;
+                    result = (input_1 + input_2);
                 }
-            }
-            else if(input[0] != null)
-            {
-                result = Int32.Parse(input[0]);
+                else if (_symbol == "-")
+                {
+                    result = (input_1 - input_2);
+                }
+                else if (_symbol == "x")
+                {
+                    result = (input_1 * input_2);
+                }
+                else if (_symbol == "/")
+                {
+                    result = (input_1 / input_2);
+                }
+                else
+                {
+                    result = input_1;
+                }
+
+                input[1] = 0;
+            
+                for (int i = 0; i < input.Length; i++)
+                {
+                    input[i] = 0;
+                }
+
+                input[0] = result;
+
+                i = 0;
+            
+                resultshow = true;
             }
 
             return result;
@@ -149,14 +143,14 @@ namespace Calculator
         
         private void clear(object sender, RoutedEventArgs e)
         {
-            input[i] = "";
-            Output.Text = input[i];
+            input[i] = 0;
+            Output.Text = "";
         }
         private void calculator_clearall(object sender, RoutedEventArgs e)
         {
             for (int i = 0; i < input.Length; i++)
             {
-                input[i] = "";
+                input[i] = 0;
             }
             
             Output.Text = "";
